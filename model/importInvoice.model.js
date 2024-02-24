45d346b75a4eb48928d60e8d778b386f83c077f1
quangnm18 <nguyenminhquang03092000@gmail.com>
2024-02-25 00:29:21 +0700

    final-commit-24022024

--- model/importInvoice.model.js
+++ model/importInvoice.model.js
@@ -329,7 +329,7 @@ ImportIv.hardDelIvDetail = function (id, callback) {
 
 ImportIv.updateIvDetail = function (data, callback) {
   db.query(
-    `UPDATE ipt_detail SET han_dung='${data.han_dung}', so_lo='${data.so_lo}', giaban_daqd=${data.giaban} WHERE id=${data.id}`,
+    `UPDATE ipt_detail SET han_dung='${data.han_dung}', so_lo='${data.so_lo}', giaban_daqd=${data.giaban}, dong_goi='${data.dong_goi}' WHERE id=${data.id}`,
     (err, res) => {
       if (err) {
         callback(err);
